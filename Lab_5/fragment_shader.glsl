0480ade17c52b3bc151d8c0f95166a65aa17efa7
xlience <vitya.speed@yandex.ru>
2025-04-27 15:10:39 +0800

    fragment_shader.glsl

--- Lab_5/fragment_shader.glsl
+++ Lab_5/fragment_shader.glsl
@@ -1,9 +1,44 @@
 #version 460 core
 
-out vec4 color;
-uniform float timeValue;
-uniform vec3 lightColor;
+out vec4 FragColor;
+in vec3 Normal;
+in vec3 FragPos;
+
+uniform vec3 viewPos;
+
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+uniform Light Light_1;
+uniform Material Mat_1;
 
 void main() {
-    color = vec4(0.5 + 0.5 * sin(timeValue), 1.0, 1.0, 0.7);
+    // Ambient
+    vec3 ambient = Light_1.ambient * Mat_1.ambient;
+    
+    // Diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(Light_1.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = Light_1.diffuse * (diff * Mat_1.diffuse);
+    
+    // Specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), Mat_1.shininess);
+    vec3 specular = Light_1.specular * (spec * Mat_1.specular);
+    
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
 }
