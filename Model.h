dd1b43e6ac9f48b72219bea2c9695ec59e4a7fea
xlience <vitya.speed@yandex.ru>
2025-05-16 22:47:33 +0800

    Update Model.h

--- Model.h
+++ Model.h
@@ -1,94 +1,83 @@
 #ifndef MODEL_H
 #define MODEL_H
-#define GLEW_DLL
 
-#include "GL/glew.h"
-#include "GLFW/glfw3.h"
-
+#include <vector>
+#include <string>
 #include <glm.hpp>
 #include <matrix_transform.hpp>
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
-
 #include "Mesh.h"
 #include "Shader.h"
 
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <iostream>
-#include <vector>
-#include <algorithm> 
-using namespace std;
-
-class Model
-{
+class Model {
 public:
-    vector<Mesh> meshes;
-    string directory;
+    std::vector<Mesh> meshes;
+    std::vector<glm::mat4> meshTransforms;
+    std::string directory;
 
-    Model(string const& path)
-    {
+    Model(std::string const& path) {
         loadModel(path);
+        meshTransforms.resize(meshes.size(), glm::mat4(1.0f));
     }
 
-    void Draw(Shader& shader)
-    {
-        for (unsigned int i = 0; i < meshes.size(); i++) {
+    void Draw(Shader& shader) {
+        for (size_t i = 0; i < meshes.size(); i++) {
+            shader.setMat4("model", meshTransforms[i]);
             meshes[i].Draw(shader);
         }
     }
 
+    void UpdateTransform(int meshIndex, const glm::mat4& transform) {
+        if (meshIndex >= 0 && meshIndex < meshTransforms.size()) {
+            meshTransforms[meshIndex] = transform;
+        }
+    }
+
 private:
-    void loadModel(string const& path)
-    {
+    void loadModel(std::string const& path) {
         Assimp::Importer importer;
         const aiScene* scene = importer.ReadFile(path,
-            aiProcess_CalcTangentSpace |
             aiProcess_Triangulate |
-            aiProcess_JoinIdenticalVertices |
-            aiProcess_SortByPType);
+            aiProcess_GenNormals |
+            aiProcess_FlipUVs);
 
-        if (nullptr == scene) {
-            cout << "ASSIMP ERROR::" << importer.GetErrorString() << endl;
+        if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+            std::cerr << "ASSIMP ERROR: " << importer.GetErrorString() << std::endl;
             return;
         }
-
+        directory = path.substr(0, path.find_last_of('/'));
         processNode(scene->mRootNode, scene);
     }
 
-    void processNode(aiNode* node, const aiScene* scene)
-    {
-        for (unsigned int i = 0; i < node->mNumMeshes; i++)
-        {
+    void processNode(aiNode* node, const aiScene* scene) {
+        for (unsigned int i = 0; i < node->mNumMeshes; i++) {
             aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
             meshes.push_back(processMesh(mesh, scene));
         }
-        for (unsigned int i = 0; i < node->mNumChildren; i++)
-        {
+
+        for (unsigned int i = 0; i < node->mNumChildren; i++) {
             processNode(node->mChildren[i], scene);
         }
     }
 
-    Mesh processMesh(aiMesh* mesh, const aiScene* scene)
-    {
-        vector<Vertex> vertices;
-        vector<unsigned int> indices;
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene) {
+        std::vector<Vertex> vertices;
+        std::vector<unsigned int> indices;
 
         for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
             Vertex vertex;
-            glm::vec3 vector;
-            vector.x = mesh->mVertices[i].x;
-            vector.y = mesh->mVertices[i].y;
-            vector.z = mesh->mVertices[i].z;
-            vertex.Position = vector;
+            vertex.Position = glm::vec3(
+                mesh->mVertices[i].x,
+                mesh->mVertices[i].y,
+                mesh->mVertices[i].z);
 
             if (mesh->HasNormals()) {
-                vector.x = mesh->mNormals[i].x;
-                vector.y = mesh->mNormals[i].y;
-                vector.z = mesh->mNormals[i].z;
-                vertex.Normal = vector;
+                vertex.Normal = glm::vec3(
+                    mesh->mNormals[i].x,
+                    mesh->mNormals[i].y,
+                    mesh->mNormals[i].z);
             }
             vertices.push_back(vertex);
         }
@@ -103,4 +92,4 @@ private:
     }
 };
 
-#endif
+#endif // MODEL_H
