820be1b8eb58678fa7489bc62c1d78b1060a60bc
xlience <vitya.speed@yandex.ru>
2025-05-16 22:46:32 +0800

    Update fragment_shader.glsl

--- fragment_shader.glsl
+++ fragment_shader.glsl
@@ -1,10 +1,17 @@
 #version 460 core
-
 out vec4 FragColor;
+
 in vec3 Normal;
 in vec3 FragPos;
 
 uniform vec3 viewPos;
+
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+}; 
 
 struct Light {
     vec3 position;
@@ -13,32 +20,25 @@ struct Light {
     vec3 specular;
 };
 
-struct Material {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
-    float shininess;
-};
-
-uniform Light Light_1;
-uniform Material Mat_1;
+uniform Material material;
+uniform Light light;
 
 void main() {
     // Ambient
-    vec3 ambient = Light_1.ambient * Mat_1.ambient;
+    vec3 ambient = light.ambient * material.ambient;
     
     // Diffuse 
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(Light_1.position - FragPos);
+    vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = Light_1.diffuse * (diff * Mat_1.diffuse);
+    vec3 diffuse = light.diffuse * (diff * material.diffuse);
     
     // Specular
     vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), Mat_1.shininess);
-    vec3 specular = Light_1.specular * (spec * Mat_1.specular);
-    
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular);  
+        
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
 }
